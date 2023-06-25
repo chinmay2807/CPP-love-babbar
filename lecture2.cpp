b10c3db93a445ddcf9a5c1e23100d69f43cf13ed
chinmay2807 <137691792+chinmay2807@users.noreply.github.com>
2023-06-25 16:13:10 +0530

    Create lecture2.cpp

---
+++ lecture2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"NAMASTE!!"<<endl;
+    cout<<"\n";
+    int a=1;
+    cout<<a<<endl;
+    char b='v';
+    cout<<b<<endl;
+    bool c=true;
+    cout<<c<<endl;
+    float d=1.2;
+    cout<<d<<endl;
+    double e=1.3;
+    cout<<e<<endl;
+    int f=sizeof(e);
+    cout<<"size of e is :"<<f<<endl;
+    int g='a';
+    cout<<g<<endl;
+    char h=98;
+    cout<<h<<endl;
+    char i=256;
+    cout<<i<<endl;
+    int j=-123456;
+    cout<<j<<endl;
+    unsigned int k=-123456;
+    cout<<j<<endl;
+    int ab=2/5;
+    cout<<ab<<endl;
+    int abc=7/5;
+    cout<<abc<<endl;
+    float bc=2.0/5;
+    cout<<bc<<endl;
+    double ac=2/5;
+    cout<<ac<<endl;
+    int a1=2;
+    int a2=3;
+    bool a11=(a1==a2);
+    bool a12=(a2>a1);
+    bool a13=(a2!=a1);
+    cout<<a11<<endl;
+    cout<<a12<<endl;
+    cout<<a13<<endl;
+    cout<<!a1<<endl;
+    cout<<(a>0&&b>0)<<endl;
+
+}
